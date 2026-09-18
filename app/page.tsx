997a6b7d1ee88417151e2f21aa714c916ec4abe2
DilushaJS <dilushashashipriya@gmail.com>
2026-09-18 23:30:04 +0530

    Tools section redesign and font issue fixed

--- app/page.tsx
+++ app/page.tsx
@@ -12,8 +12,8 @@ export default function Home() {
     <main className="min-h-screen">
       <Hero />
       <IconStrip />
-      <AdvantagesSection />
       <ToolsSection />
+      <AdvantagesSection />
       <HowItWorksSection />
       <TestimonialsSection />
       <ClientsSection />
